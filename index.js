8c313db709eb3e2482d2d930321e3cdfd6c1d73b
Your Name <you@example.com>
2021-01-14 20:40:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, callback, startingValue) {
+    let total = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+    for (; i < array.length; i++) {
+        total = callback(array[i], total)
+    }
+    return total
+}
